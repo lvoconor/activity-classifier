2a7e8c778de4550011c408ea6cf1bd7b8e1e8e86
mjbrown27 <Matt@dn800cea06.stanford.edu>
2013-11-10 22:24:27 -0800

    adding raw interial training data
    formatted nicely

--- GDA2.m
+++ GDA2.m
@@ -7,8 +7,8 @@ clc
 %% Train Model
 load CS229training_data
 
-%featuresToUse = [1 2 3 121 122 123]; %preforms terribly, need more than 6
-featuresToUse = 1:561;
+featuresToUse = [1 2 3 121 122 123]; %preforms terribly, need more than 6
+%featuresToUse = 1:561;
 X = X_train(:,featuresToUse);
 
 cls = ClassificationDiscriminant.fit(X,y_train);
