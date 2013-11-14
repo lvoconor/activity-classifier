3b8d68269a0a06abd90a3b5a7ede2998e32dbc2d
Trey Deitch <me@tdeitch.com>
2013-11-14 10:49:21 -0800

    Minor cleanup of GDA code

--- GDA.m
+++ GDA.m
@@ -1,22 +1,18 @@
 %% GDA with built-in functions
-format compact
-close all
-clear all
-clc
 
 %% Train Model
 load CS229training_data
 
-featuresToUse = [1 2 3 121 122 123]; %preforms terribly, need more than 6
-%featuresToUse = 1:561;
+%featuresToUse = [1 2 3 121 122 123]; %preforms terribly, need more than 6
+featuresToUse = 1:561;
 X = X_train(:,featuresToUse);
 
 cls = ClassificationDiscriminant.fit(X,y_train);
 
 %% Test Model
 load CS229testing_data
-predictLabel =  predict(cls,X);
-trainingError = sum(y_train==predictLabel)/length(y_train)
+trainPredictLabel =  predict(cls,X);
+trainingAccuracy = sum(y_train==trainPredictLabel)/length(y_train)
 
-predictLabel = predict(cls,X_test(:,featuresToUse));
-testingError = sum(y_test==predictLabel)/length(y_test)
+testPredictLabel = predict(cls,X_test(:,featuresToUse));
+testingAccuracy = sum(y_test==testPredictLabel)/length(y_test)
