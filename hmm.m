e99f1172e2850afdabde3771a9f1afd753e0bd9d
Trey Deitch <me@tdeitch.com>
2013-11-14 10:49:43 -0800

    Functional but poor HMM implementation
    This HMM implementation works, but it doesn't do much better than
    random on the testing data.

--- hmm.m
+++ hmm.m
@@ -1,14 +1,27 @@
-disp('Loading Data');
-load('rawTrain.mat');
-load('rawTest.mat');
-disp('Discretizing Data');
-discreteTrainX = round(rawTrainX * 10^6 + 10^7);
-discreteTest = round(rawTest * 10^6 + 10^7);
-clearvars rawTrainX rawTest;
-disp('Generating Transition and Emission Matrices');
-[trans,emis] = hmmestimate(discreteTrainX, rawTrainY);
-trans_hat = zeros(7,7);
-trans_hat(2:7,2:7) = trans;
-trans_hat(1,2:7) = 1/6;
+%% HMM on raw data
+
+numBuckets = 10^4;
+numStates = 6;
+
+load('CS229training_data.mat');
+load('CS229testing_data.mat');
+
+% Discretize data
+discreteTrainX = round((X_train + 1) * .5 * (numBuckets - 1) + 1);
+discreteTestX = round((X_test + 1) * .5 * (numBuckets - 1) + 1);
+
+% Estimate transition and emission probabilities
+pseudo_trans = ones(numStates,numStates);
+pseudo_emis = ones(numStates,numBuckets);
+[trans,emis] = hmmestimate(discreteTrainX, y_train, 'Pseudotransitions',...
+                           pseudo_trans, 'Pseudoemissions', pseudo_emis);
+
+% Update prior probabilities
+trans_hat = zeros(numStates+1,numStates+1);
+trans_hat(2:numStates+1,2:numStates+1) = trans;
+trans_hat(1,2:numStates+1) = 1/numStates;
 emis_hat = [zeros(size(emis(1,:))); emis];
-disp('Done');
+
+% Estimate states of testing data
+estimatedStates = hmmviterbi(discreteTestX,trans_hat,emis_hat);
+testingAccuracy = sum(y_test==estimatedStates')/length(y_test)
