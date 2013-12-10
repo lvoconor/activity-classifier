4f7d453ca0d26dcaba6185a4e85346889d3d0504
Trey Deitch <tdeitch@stanford.edu>
2013-12-09 17:10:52 -0800

    Break down GDA results by activity

--- GDA_basic.m
+++ GDA_basic.m
@@ -17,10 +17,28 @@ trainingAccuracy = sum(y_train==trainPredictLabel)/length(y_train)
 testPredictLabel = predict(cls,X_test(:,featuresToUse));
 testingAccuracy = sum(y_test==testPredictLabel)/length(y_test)
 
-for i=[2,4,9,10,12,13,18,20,24]
+% Break down accuracy by subject
+
+for i=unique(subject_test)'
     y = y_test(subject_test==i);
     predict = testPredictLabel(subject_test==i);
     acc = sum(y==predict)/length(y);
     num = length(y);
     fprintf('Subject: %d, Data points: %d, Accuracy: %f\n', i, num, acc);
 end
+
+% Generate matrix of real activies vs. predicted activities
+
+[~, activities] = textread('activity_labels.txt','%d %s');
+
+predict = testPredictLabel;
+vals = zeros(6,6);
+for i=unique(y_test)'
+  for j=unique(y_test)'
+    acc = sum(j==predict & y_test==i)/sum(y_test==i);
+    vals(i,j) = acc;
+  end
+end
+
+activities
+vals
